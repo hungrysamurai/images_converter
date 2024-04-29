441ac7d0f502cf4df1e491be64d5757ace7b7f36
hungry samurai <hello@hungrysamurai.ru>
2024-04-29 12:11:56 +0300

    build vs deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/",
+  // base: "",
   base: "/projects/images_converter",
   plugins: [react()],
   build: {
-    outDir: "./dist",
+    outDir: "build",
     emptyOutDir: true,
   },
 });
