bda98a80ebde220c2f678c821f06bbac819bb283
hungry samurai <hello@hungrysamurai.ru>
2025-05-06 19:25:04 +0300

    bundle analyzer added

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import { analyzer } from 'vite-bundle-analyzer';
+
+
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), analyzer()],
   build: {
     outDir: "vite_build",
     emptyOutDir: true,
